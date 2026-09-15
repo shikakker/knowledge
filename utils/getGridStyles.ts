564665035f1e4f6977426994aa3038de2e0b3d2d
Egor Denisov <designer.egor@ya.ru>
2026-09-14 20:43:34 -0400

    fix: make knowledge shell mobile-first

--- utils/getGridStyles.ts
+++ utils/getGridStyles.ts
@@ -2,6 +2,7 @@ import { css } from "emotion";
 import tokens from "@contentful/f36-tokens";
 
 export const TOPBAR_HEIGHT = "70px";
+export const SCREEN_BREAKPOINT_DESKTOP = "900px";
 export const SCREEN_BREAKPOINT_LARGE = "1600px";
 
 export const getGridStyles = () => ({
@@ -9,25 +10,40 @@ export const getGridStyles = () => ({
     height: "100vh",
     overflow: "hidden",
     gridTemplateAreas: `
-      "topbar topbar"
-      "sidebar content"
+      "topbar"
+      "sidebar"
+      "content"
     `,
+    gridTemplateRows: "auto minmax(0, 180px) minmax(0, 1fr)",
+    [`@media screen and (min-width: ${SCREEN_BREAKPOINT_DESKTOP})`]: {
+      gridTemplateAreas: `
+        "topbar topbar"
+        "sidebar content"
+      `,
+      gridTemplateRows: `${TOPBAR_HEIGHT} minmax(0, 1fr)`,
+    },
   }),
   wrapperColumns: css({
-    gridTemplateColumns: "280px auto",
+    gridTemplateColumns: "minmax(0, 1fr)",
+    [`@media screen and (min-width: ${SCREEN_BREAKPOINT_DESKTOP})`]: {
+      gridTemplateColumns: "280px minmax(0, 1fr)",
+    },
   }),
   contentColumns: css({
     display: "grid",
-    padding: `0 ${tokens.spacingL}`,
-    gridTemplateColumns: "3fr 1fr",
+    minWidth: 0,
+    padding: `0 ${tokens.spacingM}`,
+    gridTemplateColumns: "minmax(0, 1fr)",
+    [`@media screen and (min-width: ${SCREEN_BREAKPOINT_DESKTOP})`]: {
+      padding: `0 ${tokens.spacingL}`,
+      gridTemplateColumns: "minmax(0, 3fr) minmax(0, 1fr)",
+    },
   }),
-  // this style will make the content centered withou breaking the layout in big screens
   contentColumnsBigScreens: css({
     [`@media screen and (min-width: ${SCREEN_BREAKPOINT_LARGE})`]: {
       gridTemplateColumns: "1fr 720px 240px 1fr",
     },
   }),
-  // this style will "push" the content to the 2nd column in big screens
   columnStartTwo: css({
     [`@media screen and (min-width: ${SCREEN_BREAKPOINT_LARGE})`]: {
       gridColumnStart: 2,
